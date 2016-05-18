2373e8b781a42ceed8e06079381a3a9c48e9d10b
gaa7par <gaa7par@gmail.com>
2016-05-18 22:53:02 +0200

    tests added

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -7,7 +7,10 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_no_difference "User.count" do
       post users_path, user: { name: "", email: "user@invalid", password: "foo", password_confirmation: "bar" }
     end
+
     assert_template 'users/new'
+    assert_select '#error_explanation'
+    assert_select '.field_with_errors'
   end
 
   test "valid signup information" do
@@ -16,6 +19,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       post_via_redirect users_path, user: { name: "Example User", email: "user@example.com",
                                             password: "foobar", password_confirmation: "foobar" }
     end
+
     assert_template 'users/show'
+    assert_not flash.empty?
   end
 end
